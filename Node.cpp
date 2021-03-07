6ee379120435488d68207b050e1ea49dff16e025
joseaam2020 <joseaam@hotmail.com>
2021-03-07 12:10:06 -0600

    Creacion de las clases principales

---
+++ Node.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+void Node::test(){
+    cout << "Success!" << endl;
+}
---
+++ Node.h
@@ -0,0 +1,10 @@
+#ifndef Node_H_
+#define Node_H_
+
+class Node
+{
+public:
+    void test();
+};
+
+#endif /* Node_H_*/
